47dca381c674d047167e2b5076a31f16505bb9b6
watano1168 <watano1168@gmail.com>
2019-08-22 14:40:21 +0900

    Add: ダミーデータの追加

--- controllers/api/v1/posts_controller.go
+++ controllers/api/v1/posts_controller.go
@@ -4,8 +4,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var dummyData = []gin.H{
+	{
+		"user_name": "watano",
+		"content":   "Hello, World!!",
+	},
+	{
+		"user_name": "でみ",
+		"content":   "Hello, golang!!",
+	},
+	{
+		"user_name": "箒コウモリ",
+		"content":   "Hello, Adobe!!",
+	},
+	{
+		"user_name": "はたはた",
+		"content":   "Hello, Vue!!",
+	},
+}
+
+// GetPosts   GET "/api/v1/posts"
 func GetPosts(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"content": "Hello, World!!",
-	})
+	ctx.JSON(200, dummyData)
 }
