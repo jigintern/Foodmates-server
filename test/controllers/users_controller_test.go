f7d91d63f8a9869a7e2601cfba9be84d5424cf17
Kanta Demizu <demizu.1203@gmail.com>
2019-08-28 15:20:29 +0900

    [FIX] Add: TestUserSucceed/Response comparision Fix: Users Table's column

--- test/controllers/users_controller_test.go
+++ test/controllers/users_controller_test.go
@@ -11,11 +11,12 @@ import (
 
 func TestReadUserSucceed(t *testing.T) {
 	router := initialize.InitServer()
-
+	expectedResponseJSON := `{"id": 1,"created_at": "2019-08-27T07:36:43+09:00","updated_at": "2019-08-27T07:46:18+09:00","name": "hogehoge","biography": "","country": "Jap","prefecture": "Fukui","icon_address": ""}`
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/api/v1/users/1", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
+	assert.JSONEq(t, expectedResponseJSON, w.Body.String())
 }
 
 func TestReadUserFailed(t *testing.T) {
