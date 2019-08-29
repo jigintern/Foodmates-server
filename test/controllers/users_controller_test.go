2c96114519031e2e4872fbb69b1d2fd3dc251bde
Kanta Demizu <demizu.1203@gmail.com>
2019-08-29 17:48:20 +0900

    [FIX] User and Dishes controller Tests, Remove dishes_model/deleted_at column

--- test/controllers/users_controller_test.go
+++ test/controllers/users_controller_test.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/jigintern/Foodmates-server/models"
 	"io/ioutil"
 	"net/http"
@@ -56,4 +57,56 @@ func TestReadUserSucceed(t *testing.T) {
 			assert.Equal(t, testUserId, resjson.ID)
 		})
 	})
+}
+
+// TestReadAllUsers   全てのユーザー情報を取得するAPIのテスト
+func TestReadAllUsers(t *testing.T) {
+
+	// 初期処理
+
+	// 成功ケース
+	t.Run("succeed", func(t *testing.T) {
+
+		// 初期処理
+		request, err := http.NewRequest("GET", "http://localhost:8080/api/v1/dishes/readall/", nil)
+		if err != nil {
+			t.Fatalf("\x1b[31msend request failed. (%s)\x1b[0m\n", err.Error())
+		}
+		client := &http.Client{}
+		resp, err := client.Do(request)
+		if err != nil {
+			t.Fatalf("\x1b[31mrecieve request failed. (%s)\x1b[0m\n", err.Error())
+		}
+		defer resp.Body.Close()
+		resbin, err := ioutil.ReadAll(resp.Body)
+		var resjson []models.User
+		err = json.Unmarshal(resbin, &resjson)
+		if err != nil {
+			t.Fatalf("\x1b[31mjson unmarshal failed. (%s)\x1b[0m\n", err.Error())
+		}
+		t.Logf("\n\x1b[33m======= responce =======\n%+v\x1b[0m\n", resjson)
+
+		// ステータスコード200が返されること
+		t.Run("return response code 200", func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		})
+
+		// 全ての投稿データがDishの配列として返されること
+		t.Run("response type is []User", func(t *testing.T) {
+			t.Parallel()
+			assert.NotEmpty(t, resjson)
+		})
+
+		// 配列の全要素の形式が正しいこと
+		t.Run("all of response datas are correct format", func(t *testing.T) {
+			t.Parallel()
+			var resjson []models.User
+			err = json.Unmarshal(resbin, &resjson)
+			fmt.Println(resjson)
+		})
+	})
+
+	// 終了処理
+
 }
