b373caebf6d6ce92131d7cab3f4a7c5ce3584068
watano1168 <watano1168@gmail.com>
2019-08-29 10:52:51 +0900

    [ADD] 各コントローラにテストケースを追加

--- test/controllers/posts_controller_test.go
+++ test/controllers/posts_controller_test.go
@@ -6,7 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"testing"
-	"time"
+	"strconv"
+	"bytes"
 
 	"github.com/jigintern/Foodmates-server/models"
 	"github.com/stretchr/testify/assert"
@@ -16,80 +17,147 @@ import (
 func TestReadAllPosts(t *testing.T) {
 	
 	// 初期処理
-	t.Logf("\x1b[36m[TestReadAllPosts] setup: %s\x1b[0m\n", time.Now())
 	
 	// 成功ケース
 	t.Run("succeed", func(t *testing.T) {
 		
 		// 初期処理
-		t.Logf("\x1b[36m[TestReadAllPosts/success] setup: %s\x1b[0m\n", time.Now())
-		
 		request, err := http.NewRequest("GET", "http://localhost:8080/api/v1/posts/readall/", nil)
-		if err != nil {
-			t.Fatalf("\x1b[31msend request failed. (%s)\x1b[0m\n", err.Error())
-		}
+		if err != nil { t.Fatalf("\x1b[31msend request failed. (%s)\x1b[0m\n", err.Error()) }
 		client := &http.Client{}
 		resp, err := client.Do(request)
-		if err != nil {
-			t.Fatalf("\x1b[31mrecieve request failed. (%s)\x1b[0m\n", err.Error())
-		}
+		if err != nil { t.Fatalf("\x1b[31mrecieve request failed. (%s)\x1b[0m\n", err.Error()) }
+		
 		defer resp.Body.Close()
 		resbin, err := ioutil.ReadAll(resp.Body)
+		var resjson []models.Post
+		err = json.Unmarshal(resbin, &resjson)
+		if err != nil { t.Fatalf("\x1b[31mjson unmarshal failed. (%s)\x1b[0m\n", err.Error()) }
+		t.Logf("\n\x1b[33m======= responce =======\n%+v\x1b[0m\n", resjson)
 		
 		// ステータスコード200が返されること
 		t.Run("return response code 200", func(t *testing.T) {
 			t.Parallel()
 			assert.Equal(t, http.StatusOK, resp.StatusCode)
-			t.Logf("\x1b[35m[TestReadAllPosts] return response code 200: %s\x1b[0m\n", time.Now())
 		})
 
-		// 全ての投稿データが配列として返されること
-		t.Run("return response array of all posts", func(t *testing.T) {
+		// 全ての投稿データがPostの配列として返されること
+		t.Run("response type is []Post", func(t *testing.T) {
+			t.Parallel()
+			assert.NotEmpty(t, resjson)
+		})
+		
+		// 配列の全要素の形式が正しいこと
+		t.Run("all of response datas are correct format", func(t *testing.T) {
 			t.Parallel()
 			var resjson models.Post
-			err = json.Unmarshal(resbin, resjson)
+			err = json.Unmarshal(resbin, &resjson)
 			fmt.Println(resjson)
-			t.Logf("\x1b[35m[TestReadAllPosts] return response array of all posts: %s\x1b[0m\n", time.Now())
 		})
 	})
 
 	// 終了処理
-	t.Logf("\x1b[36m[TestReadAllPosts] tear-down: %s\x1b[0m\n", time.Now())
 
 }
 
-/*
-func TestCreatePostSucceed(t *testing.T) {
-	jsonStr := `{"user_id:"` + "2" + `","dish_id":"` + "6" + `","comment":"` + "fafafafafafafafafa" + `","image_address":"` + "hahaha.png" + `"}`
+// TestReadSpecificUsersPost   特定のユーザーの投稿のみを全て取得するAPIのテスト
+func TestReadSpecificUsersPost(t *testing.T) {
+	
+	// 初期処理
+	
+	// 成功ケース
+	t.Run("succeed", func(t *testing.T) {
+		
+		// 初期処理
+		testUserId := 1
+		request, err := http.NewRequest("GET", "http://localhost:8080/api/v1/posts/read/" + strconv.Itoa(testUserId), nil)
+		if err != nil { t.Fatalf("\x1b[31msend request failed. (%s)\x1b[0m\n", err.Error()) }
+		client := &http.Client{}
+		resp, err := client.Do(request)
+		if err != nil { t.Fatalf("\x1b[31mrecieve request failed. (%s)\x1b[0m\n", err.Error()) }
+		
+		defer resp.Body.Close()
+		resbin, err := ioutil.ReadAll(resp.Body)
+		var resjson []models.Post
+		err = json.Unmarshal(resbin, &resjson)
+		if err != nil { t.Fatalf("\x1b[31mjson unmarshal failed. (%s)\x1b[0m\n", err.Error()) }
+		t.Logf("\n\x1b[33m======= responce =======\n%+v\x1b[0m\n", resjson)
+		
+		// ステータスコード200が返されること
+		t.Run("return response code 200", func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		})
 
-	req, err := http.NewRequest("POST", "http://localhost:8080/api/v1/posts/create/", bytes.NewBuffer([]byte(jsonStr)))
-	if err != nil {
-		t.Fatalf("generate request failed.")
-	}
-	req.Header.Set("Content-Type", "application/json")
+		// 全ての投稿データがPostの配列として返されること
+		t.Run("response type is []Post", func(t *testing.T) {
+			t.Parallel()
+			assert.NotEmpty(t, resjson)
+		})
+		
+		// 配列の全要素の形式が正しいこと
+		t.Run("all of response datas are correct format", func(t *testing.T) {
+			t.Parallel()
+		})
+		
+		// 全ての投稿データのuser_idが指定したIDであること
+		t.Run("all of response data's user_id is expected value", func(t *testing.T) {
+			t.Parallel()
+			for i := 0; i < len(resjson); i++ {
+				assert.Equal(t, testUserId, resjson[i].UserId)
+			}
+		})
+	})
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Fatalf("send request failed.   %s", err.Error())
-	}
-	defer resp.Body.Close()
+	// 終了処理
 
-	resbin, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatalf("read responce failed.")
-	}
-	var resjson models.Post
-	json.Unmarshal(resbin, resjson)
-	fmt.Println(resjson)
 }
 
-func TestCreatePostFailed(t *testing.T) {
-	router := initialize.InitServer()
+// TestReadAllPosts   投稿を追加するAPIのテスト
+func TestCreatePost(t *testing.T) {
+	
+	// 初期処理
+	
+	// 成功ケース
+	t.Run("succeed", func(t *testing.T) {
+		
+		// 初期処理
+		testdata := models.Post{
+			UserId: 2,
+			DishId: 4,
+			Comment: "it was delicious!",
+			ImageAddress: "img/gazou/abc/123.jpg",
+		}
+		testdata_bin, err := json.Marshal(testdata)
+		if err != nil { t.Fatalf("\x1b[31mjson marshal failed. (%s)\x1b[0m\n", err.Error()) }
+		req, err := http.NewRequest("POST", "http://localhost:8080/api/v1/posts/create/", bytes.NewBuffer([]byte(testdata_bin)))
+		if err != nil { t.Fatalf("\x1b[31msend request failed. (%s)\x1b[0m\n", err.Error()) }
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil { t.Fatalf("\x1b[31mrecieve request failed. (%s)\x1b[0m\n", err.Error()) }
+		
+		defer resp.Body.Close()
+		resbin, err := ioutil.ReadAll(resp.Body)
+		var resjson models.Post
+		err = json.Unmarshal(resbin, &resjson)
+		t.Logf("\n\x1b[33m======= responce =======\n%+v\x1b[0m\n", resjson)
+		
+		// ステータスコード200が返されること
+		t.Run("return response code 200", func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		})
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/api/v1/posts/create/", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusBadRequest, w.Code)
+		// 送信したデータが返されること
+		t.Run("return response array of all posts", func(t *testing.T) {
+			t.Parallel()
+		})
+		
+		// ReadAllPostsで、作成した内容のデータが末尾に追加されて返ってくること
+		t.Run("when read all posts, created data is returned at last of array", func(t *testing.T) {
+		})
+	})
+
+	// 終了処理
+
 }
-*/
