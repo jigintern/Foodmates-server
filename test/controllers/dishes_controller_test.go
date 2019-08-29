8dea93a443ce046340458bb934654ba5b9d6c797
Kanta Demizu <demizu.1203@gmail.com>
2019-08-29 16:57:12 +0900

    [FIX] testReadAllDishes

--- test/controllers/dishes_controller_test.go
+++ test/controllers/dishes_controller_test.go
@@ -1,20 +1,63 @@
 package controllers
 
-/*
 import (
-	"github.com/jigintern/Foodmates-server/test/initialize"
+	"encoding/json"
+	"fmt"
+	"github.com/jigintern/Foodmates-server/models"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
-func TestReadAllDishesSucceed(t *testing.T) {
-	router := initialize.InitServer()
+// TestReadAllPosts   全ての投稿を取得するAPIのテスト
+func TestReadAllDishes(t *testing.T) {
 
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/api/v1/dishes/readall/", nil)
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
+	// 初期処理
+
+	// 成功ケース
+	t.Run("succeed", func(t *testing.T) {
+
+		// 初期処理
+		request, err := http.NewRequest("GET", "http://localhost:8080/api/v1/dishes/readall/", nil)
+		if err != nil {
+			t.Fatalf("\x1b[31msend request failed. (%s)\x1b[0m\n", err.Error())
+		}
+		client := &http.Client{}
+		resp, err := client.Do(request)
+		if err != nil {
+			t.Fatalf("\x1b[31mrecieve request failed. (%s)\x1b[0m\n", err.Error())
+		}
+		defer resp.Body.Close()
+		resbin, err := ioutil.ReadAll(resp.Body)
+		var resjson []models.Dish
+		err = json.Unmarshal(resbin, &resjson)
+		if err != nil {
+			t.Fatalf("\x1b[31mjson unmarshal failed. (%s)\x1b[0m\n", err.Error())
+		}
+		t.Logf("\n\x1b[33m======= responce =======\n%+v\x1b[0m\n", resjson)
+
+		// ステータスコード200が返されること
+		t.Run("return response code 200", func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+		})
+
+		// 全ての投稿データがDishの配列として返されること
+		t.Run("response type is []Post", func(t *testing.T) {
+			t.Parallel()
+			assert.NotEmpty(t, resjson)
+		})
+
+		// 配列の全要素の形式が正しいこと
+		t.Run("all of response datas are correct format", func(t *testing.T) {
+			t.Parallel()
+			var resjson models.Post
+			err = json.Unmarshal(resbin, &resjson)
+			fmt.Println(resjson)
+		})
+	})
+
+	// 終了処理
+
 }
-*/
