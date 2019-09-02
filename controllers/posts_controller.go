682014efe52035a9763c1f2f6bf20522f3ca656f
Kanta Demizu <demizu.1203@gmail.com>
2019-09-02 14:40:57 +0900

    [FIX] Fix response of post_controller order by descending created_at column

--- controllers/posts_controller.go
+++ controllers/posts_controller.go
@@ -11,17 +11,17 @@ import (
 // ReadPosts   GET "/api/v1/posts/readall"
 func ReadAllPosts(ctx *gin.Context) {
 	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	var result []models.PostResponse
+	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	var results []models.PostResponse
 	db, err := models.GetDB()
-
 	// DBがなければ500を返す
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError})
 		return
 	}
-	db.Raw("SELECT * FROM `Posts` left outer join `Users` on `Posts`.`user_id` = `Users`.`id` left outer join `Dishes` on `Posts`.`dish_id` = `Dishes`.`id`").Scan(&result)
-	fmt.Println(result)
-	ctx.JSON(http.StatusOK, result)
+	db.Raw("SELECT Posts.id, Posts.`user_id`, Posts.`created_at`, Posts.`updated_at`, Posts.`dish_id`, Posts.`comment`, Posts.`image_address`, Users.`name`, Users.`biography`, Users.`birth`, Users.`country`, Users.`prefecture`, Users.`icon_address`, Dishes.`dish_name`, Dishes.`store_name` FROM `Posts` LEFT OUTER JOIN `Users` ON `Posts`.`user_id` = `Users`.`id` LEFT OUTER JOIN `Dishes` ON `Posts`.`dish_id` = `Dishes`.`id` ORDER BY Posts.created_at DESC").Scan(&results)
+	fmt.Println(results)
+	ctx.JSON(http.StatusOK, results)
 }
 
 // ReadPost   GET "/api/v1/posts/read/:user_id"
@@ -41,7 +41,7 @@ func ReadSpecificUsersPost(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError})
 		return
 	}
-	db.Raw("SELECT * FROM `Posts` left outer join Users on Posts.user_id = Users.id left outer join Dishes on Posts.dish_id = Dishes.id WHERE user_id = ? ", id).Scan(&results)
+	db.Raw("SELECT Posts.id, Posts.`user_id`, Posts.`created_at`, Posts.`updated_at`, Posts.`dish_id`, Posts.`comment`, Posts.`image_address`, Users.`name`, Users.`biography`, Users.`birth`, Users.`country`, Users.`prefecture`, Users.`icon_address`, Dishes.`dish_name`, Dishes.`store_name` FROM `Posts` LEFT OUTER JOIN `Users` ON `Posts`.`user_id` = `Users`.`id` LEFT OUTER JOIN `Dishes` ON `Posts`.`dish_id` = `Dishes`.`id` WHERE Posts.user_id = ? ORDER BY Posts.created_at DESC", id).Scan(&results)
 	ctx.JSON(http.StatusOK, results)
 }
 
@@ -65,6 +65,7 @@ func CreatePost(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"status": "starting transition failed."})
 		return
 	}
+	//db.Table("Posts").Create(&param)
 	if len(db.GetErrors()) != 0 {
 		fmt.Println("\x1b[31mchanging database failed.\x1b[0m")
 		tx.Rollback()
