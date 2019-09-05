3150d4d0ac7d6c0f030d119d4cb631231bcd80ee
Kanta Demizu <demizu.1203@gmail.com>
2019-09-05 10:11:34 +0900

    [FIX] SignUp/Submit icon_address

--- models/users_model.go
+++ models/users_model.go
@@ -19,10 +19,11 @@ type User struct {
 }
 
 type SignUpParams struct {
-	LoginName  string    `gorm:"default:''" json:"login_name"`
-	Name       string    `gorm:"default:''" json:"name"`
-	Biography  string    `gorm:"default:''" json:"biography"`
-	Birth      time.Time `json:"birth" sql:"type:date" time_format:"2006-01-02"`
-	Country    string    `gorm:"default:''" json:"country"`
-	Prefecture string    `gorm:"default:''" json:"prefecture"`
+	LoginName   string    `gorm:"default:''" json:"login_name"`
+	Name        string    `gorm:"default:''" json:"name"`
+	Biography   string    `gorm:"default:''" json:"biography"`
+	Birth       time.Time `json:"birth" sql:"type:date" time_format:"2006-01-02"`
+	Country     string    `gorm:"default:''" json:"country"`
+	Prefecture  string    `gorm:"default:''" json:"prefecture"`
+	IconAddress string    `gorm:"default:''" json:"icon_address"`
 }
