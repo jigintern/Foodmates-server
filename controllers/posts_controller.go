abf2f8094b16a449a25ab1b0c38a722696ee2066
Kanta Demizu <demizu.1203@gmail.com>
2019-09-05 14:00:02 +0900

    [FIX] using login_name on ReadSpecificUsersPost

--- controllers/posts_controller.go
+++ controllers/posts_controller.go
@@ -52,11 +52,11 @@ func ReadSpecificUsersPost(ctx *gin.Context) {
 		return
 	}
 	var userData models.User
-	recordNotFound := db.Table("Users").Select("Users.login_name").Where("login_name=?", loginName).First(&userData).RecordNotFound()
+	recordNotFound := db.Table("Users").Where("login_name=?", loginName).First(&userData).RecordNotFound()
 	if recordNotFound {
 		ctx.JSON(http.StatusInternalServerError, nil)
 	} else {
-		db.Raw("SELECT Posts.id, Posts.`user_id`, Posts.`created_at`, Posts.`updated_at`, Posts.`dish_id`, Posts.`comment`, Posts.`image_address`, Users.`name`, Users.`biography`, Users.`birth`, Users.`country`, Users.`prefecture`, Users.`icon_address`, Dishes.`dish_name`, Dishes.`store_name` FROM `Posts` LEFT OUTER JOIN `Users` ON `Posts`.`user_id` = `Users`.`id` LEFT OUTER JOIN `Dishes` ON `Posts`.`dish_id` = `Dishes`.`id` WHERE Posts.user_id = ? ORDER BY Posts.created_at DESC", userData.ID).Scan(&results)
+		db.Raw("SELECT Posts.id, Posts.`user_id`, Posts.`created_at`, Posts.`updated_at`, Posts.`dish_id`, Posts.`comment`, Posts.`image_address`, Users.`login_name`, Users.`name`, Users.`biography`, Users.`birth`, Users.`country`, Users.`prefecture`, Users.`icon_address`, Dishes.`dish_name`, Dishes.`store_name` FROM `Posts` LEFT OUTER JOIN `Users` ON `Posts`.`user_id` = `Users`.`id` LEFT OUTER JOIN `Dishes` ON `Posts`.`dish_id` = `Dishes`.`id` WHERE Posts.user_id = ? ORDER BY Posts.created_at DESC", userData.ID).Scan(&results)
 		ctx.JSON(http.StatusOK, results)
 	}
 }
